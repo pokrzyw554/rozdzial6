98f8f49999429e2fed69244c688ce179b50e4be7
krzychu <ojebalem.cale@gmail.com>
2017-07-14 13:21:00 +0000

    cos zjebalem

--- 6_8.c
+++ 6_8.c
@@ -9,17 +9,18 @@ int main(void)
 	printf("teraz druga\n");
 	cos = scanf("%f", &druga);
 	printf("no to napierdalamy!\n");
-	while(cos=1)
+	while(cos==1)
 		{
 		iloczyn = piersza * druga;
 		roznica = piersza - druga;
 		wynik = roznica / iloczyn;	//owy wynik sie nie wyswietla
-		printf("%f o to kurwa wynik xD\n");
+		printf("%f %f %f %f\n", piersza, druga, iloczyn, roznica);
+		printf("%f o to kurwa wynik xD\n", wynik);
 		printf("dawaj kolejne liczby lub (q)uit\n");
 		cos = scanf("%f", &piersza);
 		printf("nastepna!\n");
 		scanf("%f", &druga);
-		}		//ta petla sie zjebala
+		}		//dziala
 	printf("konczymy program....\n");
 	return 0;
 }
